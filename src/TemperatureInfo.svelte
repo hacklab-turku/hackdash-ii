7c8b0f59a812540cc31671e98919ed89facf87c8
Skyler Mäntysaari <samip5@users.noreply.github.com>
2020-11-29 14:01:53 +0200

    feat(temp&humidity): Temperautere and humidity works.

--- src/TemperatureInfo.svelte
+++ src/TemperatureInfo.svelte
@@ -11,62 +11,32 @@ p {
 
 <script>
     import config from "./settings/EndpointSettings";
+    import { onInterval } from './utils.js';
+
     let firstTemperature;
     let firstHumidity;
 
-    $.when(
-        $.get(config.apiLocation+'temp/', {a: 'getTemp'}, function (response) {
-            firstTemperature = response.data;
-            log(firstTemperature);
-        }, 'JSON'),
-        $.get(config.apiLocation+'humidity/', {a: 'getHumidity'}, function (response) {
-            firstHumidity = response.data;
-            log(firstHumidity);
-        }, 'JSON')
-
-    ).then(function() {
-        let temperatureData = {
-            labels: ['',''],
-            datasets: [
-                {
-                    label: 'Temperature',
-                    data: [firstTemperature, firstTemperature]
-                }
-            ]
-        };
-
-        let humidityData = {
-            labels: ['',''],
-            datasets: [{
-                label: 'Humidity',
-                data: [firstHumidity, firstHumidity]
-            }]
-        };
-
-        const options = {
-            scaleShowGridLines : true,
-            scaleGridLineColor : 'rgba('+config.color+',0.1)',
-            scaleGridLineWidth : 1,
-            scaleShowHorizontalLines: true,
-            scaleShowVerticalLines: false,
-            bezierCurve : true,
-            bezierCurveTension : 0,
-            pointDot : true,
-            pointDotRadius : 3,
-            pointDotStrokeWidth : 1,
-            pointHitDetectionRadius : 20,
-            datasetStroke : true,
-            datasetStrokeWidth : 2,
-            datasetFill : true,
-            legendTemplate : '<ul class=\'<%=name.toLowerCase()%>-legend\'><% for (var i=0; i<datasets.length; i++){%><li><span style=\'background-color:<%=datasets[i].strokeColor%>\'></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>'
-        };
-    })
-
-    let addTemperature = function(reading){
-        log('addTemperature')
-
+    async function refreshTemperature() {
+            return fetch(config.apiLocation+'temp/?a=getTemp').then(async response => {
+                firstTemperature = (await response.json()).data;
+                console.log("Temperature value: " +firstTemperature);
+            })
     }
+    async function refreshHumidity() {
+        return fetch(config.apiLocation+'humidity/?a=getHumidity').then(async response => {
+            firstHumidity = (await response.json()).data;
+            console.log("Humidity value: " +firstHumidity);
+        })
+    }
+
+    onInterval(refreshTemperature(), 30000);
+    refreshTemperature();
+    onInterval(refreshHumidity(), 30000);
+    refreshHumidity();
 </script>
 
 <h2>Temperature and humidity</h2>
-<p class="TemperatureData"></p>
+<p>
+    Temperature: {firstTemperature} C. <br>
+    Humidity: {firstHumidity} %.
+</p>
